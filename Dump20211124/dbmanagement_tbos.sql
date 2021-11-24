670bff8d5645a259c5c687be472f6991005a6fbf
Bruno Ferrari <brunofvincensi@gmail.com>
2021-11-24 14:02:34 -0300

    database diagram

---
+++ Dump20211124/dbmanagement_tbos.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dbmanagement
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbos`
+--
+
+DROP TABLE IF EXISTS `tbos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbos` (
+  `os` int NOT NULL AUTO_INCREMENT,
+  `date_os` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `equipment` varchar(150) NOT NULL,
+  `defect` varchar(150) NOT NULL,
+  `service` varchar(150) DEFAULT NULL,
+  `technician` varchar(30) DEFAULT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `idcust` int NOT NULL,
+  PRIMARY KEY (`os`),
+  KEY `idcust` (`idcust`),
+  CONSTRAINT `tbos_ibfk_1` FOREIGN KEY (`idcust`) REFERENCES `tbcustomer` (`idcust`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbos`
+--
+
+LOCK TABLES `tbos` WRITE;
+/*!40000 ALTER TABLE `tbos` DISABLE KEYS */;
+INSERT INTO `tbos` VALUES (1,'2021-11-24 00:58:34','Laptop','it do not turn on','adapter exchange','Zé',87.50,1);
+/*!40000 ALTER TABLE `tbos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-24 13:44:45
